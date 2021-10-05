6464499ac081e479517a6cc2825b5588cdcd9446
Nebeyu Zewdie <nebeyu.zewdie@gmail.com>
2021-10-05 16:12:40 -0500

    Lab 6 solutions

---
+++ src/Lab6/Q2.java
@@ -0,0 +1,24 @@
+//package Lab6;
+//
+//public class Q2 {
+//
+//    public static void main(String[] args){
+//        // a & c are equivalents
+//
+//     a.   if (a==b)
+//            if (c==d) a=1;
+//              else b =1;
+
+//     b. if (a==b){
+////            if (c==d) a=1;}
+////            else b =1;
+//
+//
+//    c.    if (a==b)
+//
+//            if(c==d) a=1;
+//
+//         else
+//            b=1;
+//    }
+//}
---
+++ src/Lab6/Q3.java
@@ -0,0 +1,23 @@
+//package Lab6;
+//
+//public class Q3 {
+//
+////
+//public static void main(String [] args){
+////
+//      int x = 10;
+//     int  y = 20;
+//     int  z = 30;
+//
+//      a. x<10|| x >10 false
+//
+////       b. x > y && y > x // false, regardless of the values this is always false
+////       c. (x < y + z) && (x + 10 <= 20) // true
+////       d. z - y == x && Math.abs(y-z) == x // true
+////       e.  x < 10 &&x> 10 // false regardless of values this is always false
+////       f.  x > y || y > x // true
+////       g. !(x < y + z) || !(x + 10 <= 20 ) //false
+////        h. !(x == y)) && (x != y) && (x<y || y<x) // true
+////}
+////}
+//// regardless of values b and e will always be false
